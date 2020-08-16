151c6b76a8dc9469165289f3b86b07f4cfcb4585
IgorGarciaCosta <igonildo7@gmail.com>
2020-08-16 12:24:17 -0300

    read function

--- notes.js
+++ notes.js
@@ -9,26 +9,40 @@ const listNote = () => {
     const notes = loadNotes()
     console.log(chalk.blue.inverse('These are your notes, my friend: \n\n'))
     notes.forEach(note => {
-        console.log('Title: ' + note.title + '\nBody: ' + note.body+'\n\n')
+        console.log('Title: ' + note.title + '\nBody: ' + note.body + '\n\n')
     });
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const foundNote = notes.find((note) => note.title === title)//busca a nota por título
+    if (foundNote) {//se tem uma nota duplicada
+        console.log(chalk.green.bold.inverse('Note Found!'))
+        console.log('\nTitle: ' + foundNote.title + '\nBody: ' + foundNote.body)
+    } else {// se tiver
+        console.log(chalk.red.bold.inverse('Note not found!'))
+    }
+
 }
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    //const duplicateNotes = notes.filter((note) => note.title === title)
     //CAN BE TRANSLATED IN:
     // const duplicateNotes = notes.filter(function (note) {
     //     return note.title === title
     // })
 
-    if (duplicateNotes.length === 0) {
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if (!duplicateNote) {//se não tem uma nota duplicada
         notes.push({
             title: title,
             body: body
         })
         saveNotes(notes)
         console.log(chalk.green.bold.inverse('New note added!'))
-    } else {
+    } else {// se tiver
         console.log(chalk.red.bold.inverse('Note title taken!'))
     }
 }
@@ -65,8 +79,8 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNote: listNote
+    listNote: listNote,
+    readNote: readNote
 }
